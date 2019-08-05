db72d5f12e2981ab5a82c080469ce9caeb525274
Nhan <nguyentrongnhanc8@gmail.com>
2019-08-05 11:28:42 +0700

    update component carousel for file JS

--- effect.js
+++ effect.js
@@ -66,5 +66,26 @@ function myFunction() {
 }
 
 //==============================================================
-
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    margin: 10,
+    responsiveClass:true,
+    responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:1,
+          nav:false
+      },
+      1000:{
+          items:2,
+          nav:true,
+          loop:false
+      }
+    }
+  });
+});
 //==============================================================
